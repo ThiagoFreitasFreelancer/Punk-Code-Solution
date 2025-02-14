83ba0f30c304eeff5c06a8f712b22b530172ee46
Thiago Freitas <thiago.goncalves@cepedi.org.br>
2025-02-14 20:51:03 -0300

    Correçções

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HerosComponent } from "./heros/heros.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HerosComponent]
 })
 export class AppComponent {
   title = 'Punk Code Solutio';
---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heros',
+  imports: [],
+  templateUrl: './heros.component.html',
+  styleUrl: './heros.component.css'
+})
+export class HerosComponent {
+
+  hero = "Batman"
+
+}
